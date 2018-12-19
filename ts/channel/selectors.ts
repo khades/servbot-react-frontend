715c67c66f6fdde2436c44d1488695c7b75afc7d
khades <khades@khades.org>
2018-12-19 20:34:18 +0700

    RC2 selector fix

--- ts/channel/selectors.ts
+++ ts/channel/selectors.ts
@@ -1,4 +1,8 @@
 import { IUserInfoState } from "../userinfo/reducer";
 
-export const getIfUserIsModerator = (userInfo: IUserInfoState, currentChannel: string): boolean =>
-    userInfo.modChannels.some((channelInfo): boolean => channelInfo.channelID === currentChannel);
+export const getIfUserIsModerator = (userInfo: IUserInfoState, currentChannel: string): boolean => {
+    if (!!userInfo && !!userInfo.modChannels) {
+        return userInfo.modChannels.some((channelInfo): boolean => channelInfo.channelID === currentChannel);
+    }
+    return false;
+};
