822e7dca5bc8be69d213dcb002dd66a7c87ea8b6
khades <khades@khades.org>
2018-12-23 21:37:57 +0700

    RC2 Default language is taken from config

--- ts/l10n/l10n.ts
+++ ts/l10n/l10n.ts
@@ -2,6 +2,7 @@ import LocalizedStrings from "react-localization";
 import l10nEN from "./l10n-en";
 import l10nRU from "./l10n-ru";
 import { IL10nStrings } from "./l10nString";
+import { config } from "../../config";
 
 export const l10n: IL10nStrings = new LocalizedStrings({
     "en-US": l10nEN,
@@ -11,12 +12,16 @@ export const l10n: IL10nStrings = new LocalizedStrings({
 const lang = localStorage.getItem("lang");
 
 if (lang == null) {
-    l10n.setLanguage("en-US");
+    if (l10n.getAvailableLanguages().some((l10nlang) => l10nlang === config.defaultLang)) {
+        l10n.setLanguage(config.defaultLang);
+    } else {
+        l10n.setLanguage("en-US");
+    }
 } else {
     if (l10n.getAvailableLanguages().some((l10nlang) => l10nlang === lang)) {
         l10n.setLanguage(lang);
     } else {
-        l10n.setLanguage("en-US");
+        l10n.setLanguage(config.defaultLang);
     }
 }
 
